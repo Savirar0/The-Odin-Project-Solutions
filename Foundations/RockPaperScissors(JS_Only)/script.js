6a0f2b27c89d2337cc9d28d9203f835c35c018ea
Navadeep <navdeep2666@gmail.com>
2026-01-21 02:49:00 +0530

    added UI o rpc using DOM

--- Foundations/RockPaperScissors(JS_Only)/script.js
+++ Foundations/RockPaperScissors(JS_Only)/script.js
@@ -1,14 +1,43 @@
+const g = ["ROCK","Scissors","Paper"];
+let count=0;
+let hS=0;
+let cS=0;
+
 function playRound(humanChoice, computerChoice) {
+    const resultDiv=document.querySelector("#result");
+    
+    count+=1;
+    
     if (humanChoice === computerChoice) {
-        return "Retry";
+        count-=1;
+        resultDiv.textContent = `Human choice: ${g[humanChoice]}, Computer choice: ${g[computerChoice]}
+        RESULT: RETRY`;
+        
+    } else if (humanChoice === 0 && computerChoice === 1) {
+        hS+=1;
+        resultDiv.textContent = `Human choice: ${g[humanChoice]}, Computer choice: ${g[computerChoice]}
+        RESULT: YOU WIN`;
     } else if (humanChoice === 1 && computerChoice === 2) {
-        return "You win";
-    } else if (humanChoice === 2 && computerChoice === 3) {
-        return "You win";
-    } else if (humanChoice === 3 && computerChoice === 1) {
-        return "You win";
+        hS+=1;
+        resultDiv.textContent = `Human choice: ${g[humanChoice]}, Computer choice: ${g[computerChoice]}
+        RESULT: YOU WIN`;
+    } else if (humanChoice === 2 && computerChoice === 0) {
+        hS+=1;
+        resultDiv.textContent = `Human choice: ${g[humanChoice]}, Computer choice: ${g[computerChoice]}
+        RESULT: YOU WIN`;
     } else {
-        return "You lose";
+        cS+=1;
+        resultDiv.textContent = `Human choice: ${g[humanChoice]}, Computer choice: ${g[computerChoice]}
+        RESULT: YOU LOSE`;
+    }
+
+    if(count===5){
+        const finalDiv = document.querySelector("#final");
+        if(hS>cS){
+            finalDiv.textContent = `${hS}/5 YOU WIN!!`;
+        }else{
+            finalDiv.textContent = `${cS}/5 YOU LOSE!!`;
+        }
     }
 }
 
@@ -19,21 +48,17 @@ function getRandomInt(max) {
 function getComputerChoice() {
     const val = getRandomInt(30);
     if (val <= 10) {
-        return 1; // Rock
+        return 0; 
     } else if (val <= 20) {
-        return 2; // Scissors
+        return 1; 
     } else {
-        return 3; // Paper
+        return 2; 
     }
 }
 
-function getHumanChoice() {
-    const humanVal = parseInt(window.prompt("Enter 1 for Rock, 2 for Scissors, and 3 for Paper"));
-    return humanVal;
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-const result = playRound(humanSelection, computerSelection);
-console.log(`Human choice: ${humanSelection}, Computer choice: ${computerSelection}`);
-console.log(result);
+const rock = document.querySelector("#rock");
+rock.onclick = () => playRound(0,getComputerChoice());
+const paper = document.querySelector("#paper");
+paper.onclick = () => playRound(2,getComputerChoice());
+const scissors = document.querySelector("#scissors");
+scissors.onclick = () => playRound(1,getComputerChoice());
