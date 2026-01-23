d8fd4f63049142d11619593b760ba8b9a9a15e63
Navadeep <navdeep2666@gmail.com>
2026-01-23 20:00:18 +0530

    makedivs() with errors

--- Foundations/etchASketch/script.js
+++ Foundations/etchASketch/script.js
@@ -1,16 +1,42 @@
-
 const button = document.querySelector(".entry");
+const container = document.querySelector(".container");
+
 const myPopup = new Popup({
     id: "resizePopUp",
     title: "Resize!",
-    content: `<div style="display: flex; flex-direction: column; gap: 10px; align-items: center;">
+    content: `
+        <div style="display: flex; flex-direction: column; gap: 10px; align-items: center;">
             <p>Enter a number between 1 and 100</p>
             <input type="number" id="sizeInput" placeholder="e.g. 16" style="padding: 5px;">
             <button id="confirmSize" style="padding: 5px 10px;">Create Grid</button>
         </div>
     `,
 });
+
 button.addEventListener('click', () => {
     myPopup.show();
+    const ch = document.querySelector("#confirmSize");
+    const input = document.querySelector("#sizeInput");
+    ch.onclick = () => {
+        const n = input.value; 
+        makeDivs(n);
+    };
 });
-let n = Document.querySelector("#inputSize").value;
+
+function makeDivs(n) {
+    let twh = 960;
+    myPopup.hide();
+    container.innerHTML = '';
+    let defh = `${twh/n}px`;
+    let defw = `${twh/n}px`;
+
+    for (let i = 0; i < n * n; i++) {
+        const box = document.createElement("div");
+        box.style.height = defh;
+        box.style.width = defw;
+        box.style.border="1px solid white";
+        box.style.background = "black";
+        
+        container.appendChild(box); 
+    }
+}
