3cdc62323d15da9a3bd71aba5762e8e2bf8ff2d3
Navadeep <navdeep2666@gmail.com>
2026-01-23 20:58:54 +0530

    makedivs() using nested loops,working

--- Foundations/etchASketch/script.js
+++ Foundations/etchASketch/script.js
@@ -24,19 +24,34 @@ button.addEventListener('click', () => {
 });
 
 function makeDivs(n) {
-    let twh = 960;
+    let twh = 800;
     myPopup.hide();
     container.innerHTML = '';
     let defh = `${twh/n}px`;
     let defw = `${twh/n}px`;
 
-    for (let i = 0; i < n * n; i++) {
-        const box = document.createElement("div");
-        box.style.height = defh;
-        box.style.width = defw;
-        box.style.border="1px solid white";
-        box.style.background = "black";
-        
-        container.appendChild(box); 
+    for(let i=0;i<n;i++){
+        const mainBox = document.createElement("div");
+        mainBox.style.display = "flex";
+        mainBox.style.flexDirection = "row";
+        container.appendChild(mainBox);
+        for(let j=0;j<n;j++){
+            const box = document.createElement("div");
+            box.style.height = defh;
+            box.style.width = defw;
+            box.style.border="1px solid white";
+            box.style.background = "black";
+            mainBox.appendChild(box);
+        }
     }
+
+    // for (let i = 0; i < n * n; i++) {
+    //     const box = document.createElement("div");
+    //     box.style.height = defh;
+    //     box.style.width = defw;
+    //     box.style.border="1px solid white";
+    //     box.style.background = "black";
+        
+    //     container.appendChild(box); 
+    // }
 }
