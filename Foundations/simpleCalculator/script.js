575ffea72bd6197b20e32e62d0401aaebb176f17
Navadeep <navdeep2666@gmail.com>
2026-01-25 20:33:16 +0530

    updated js

--- Foundations/simpleCalculator/script.js
+++ Foundations/simpleCalculator/script.js
@@ -1,18 +1,19 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".num, .zero"); 
 const operator = document.querySelectorAll(".op");
-let cnt =0;
+let numbers =[];
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         display.textContent += button.textContent; 
+        numbers.push(Number(button.textContent));
     });
 });
 
 operator.forEach(op => {
     op.addEventListener('click', ()=>{
         const operation = op.textContent;
-
+        numbers.push(op.textContent);
         display.textContent+=op.textContent;
     })
 })
-
+console.log(numbers);
