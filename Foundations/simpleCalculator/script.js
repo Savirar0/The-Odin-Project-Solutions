7997d9f19e74a6fd73a2b9a966027203720c71aa
Navadeep <navdeep2666@gmail.com>
2026-01-25 20:26:46 +0530

    better classes and ids

--- Foundations/simpleCalculator/script.js
+++ Foundations/simpleCalculator/script.js
@@ -1,9 +1,18 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".num, .zero"); 
-
+const operator = document.querySelectorAll(".op");
+let cnt =0;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         display.textContent += button.textContent; 
     });
 });
+
+operator.forEach(op => {
+    op.addEventListener('click', ()=>{
+        const operation = op.textContent;
+
+        display.textContent+=op.textContent;
+    })
+})
 
