ce668445b022a9052869bc657ba379efb2306a8e
Navadeep <navdeep2666@gmail.com>
2026-01-25 22:27:29 +0530

    equal btn event listener added

--- Foundations/simpleCalculator/script.js
+++ Foundations/simpleCalculator/script.js
@@ -1,19 +1,38 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll(".num, .zero"); 
 const operator = document.querySelectorAll(".op");
+const eq = document.getElementById("equal");
 let numbers =[];
+let curBuff = "";
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         display.textContent += button.textContent; 
-        numbers.push(Number(button.textContent));
+        curBuff+=button.textContent;
     });
 });
 
 operator.forEach(op => {
     op.addEventListener('click', ()=>{
         const operation = op.textContent;
+        if(curBuff != ""){
+            numbers.push(Number(curBuff));
+            curBuff="";
+            
+        }
         numbers.push(op.textContent);
         display.textContent+=op.textContent;
     })
+});
+
+eq.addEventListener('click', ()=>{
+    numbers.push(Number(curBuff));
+    res=0;
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i]=="+"){
+            
+        }
+    }
 })
+
+
 console.log(numbers);
